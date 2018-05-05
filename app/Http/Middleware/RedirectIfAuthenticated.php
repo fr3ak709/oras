d2d675686f1d8e825779bd039dbc92a4fdca71f9
Mindaugas Bernotas <mindaugas463@gmail.com>
2018-05-06 00:42:35 +0300

    a

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,10 +17,11 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
+        $this->app['request']->server->set('HTTPS', true);
+        /* if (Auth::guard($guard)->check()) {
             return redirect('/home');
         }
 
-        return $next($request);
+        return $next($request); */
     }
 }
